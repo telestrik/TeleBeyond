d4765d795b2345e9362d6d95e9a5c30328f0ff85
SAMBAT1 <ABOL.ANVARI96@GMAIL.COM>
2017-02-16 06:43:08 -0800

    Update jomlak.lua

--- plugins/jomlak.lua
+++ plugins/jomlak.lua
@@ -2,7 +2,7 @@ local database = 'http://vip.opload.ir/vipdl/95/1/amirab248/'
 local function run(msg)
  local res = http.request(database.."jomlak.db")
  local jomlak = res:split(",") 
- local text = jomlak[math.random(#jomlak)]..'\n\n@BeyondTeam'
+ local text = jomlak[math.random(#jomlak)]..'\n\n@iD_SHOMARE_MAJaZE_iD'
 return reply_msg(msg.id, text, ok_cb, false)
 end
 return {
