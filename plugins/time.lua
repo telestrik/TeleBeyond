e11e749dd3b02f6047b2d66b25642691eddcb086
SAMBAT1 <ABOL.ANVARI96@GMAIL.COM>
2017-02-16 06:36:39 -0800

    Update time.lua

--- plugins/time.lua
+++ plugins/time.lua
@@ -2,7 +2,9 @@ function run(msg, matches)
 local url , res = http.request('http://api.gpmod.ir/time/')
 if res ~= 200 then return "No connection" end
 local jdat = json:decode(url)
-local text = '🕒 ساعت '..jdat.FAtime..' \n📅 امروز '..jdat.FAdate..' میباشد.\n    ----\n🕒TIME '..jdat.ENtime..'\n📆 '..jdat.ENdate.. '\n@BeyondTeam'
+local text = '🕒 ساعت '..jdat.FAtime..' \n📅 امروز '..jdat.FAdate..' میباشد.\n    ----\n🕒TIME '..jdat.ENtime..'\n📆 '..jdat.ENdate.. '\n#ID SUDO: @zZz_ADMIN_SHOMARE_MAJAZEE_zZz
+#ID Channel: @iD_SHOMARE_MAJaZE_iD
+منتظر شمادوستان گل هستیم ❤️🌹💋'
 return text
 end
 return {
